8f7500d177e63f3bec1a0c80b3df3fbf0946f927
Victor Perez <vsouzaperez@gmail.com>
2024-06-21 14:00:50 -0300

    final counter

--- src/Counter.js
+++ src/Counter.js
@@ -1,13 +1,37 @@
-import React from "react";
+import React, { useState } from "react";
 import './counter.css'
 
 function Counter() {
+     const [counter, setCounter] = useState(0)
+
+     // const handleclick = operator => {
+     //      if(handleclick('-')){
+     //           console.log('teste')
+     //      }
+     // }
+
+     function handleclick(operator) {
+          // let newValue = operator 
+          // if (operator === '+') {
+          //      newValue = counter + 1
+          // }
+          // else {
+          //      newValue = counter - 1
+          // }
+
+          let newValue = operator === '+'
+               ? counter + 1
+               : counter - 1
+
+          setCounter(newValue)
+     }
+
      return (
           <div className="counter">
-               <span>0</span>
+               <span>{counter}</span>
                <div className="buttons">
-                    <button>-</button>
-                    <button>+</button>
+                    <button onClick={() => handleclick('-')}>-</button>
+                    <button onClick={() => handleclick('+')}>+</button>
                </div>
           </div>
      )
